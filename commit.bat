7d4dc2f42d37b7b9d318d01e1c669c7fe764878e
trhanhtu <tr.ho.anh.tu@gmail.com>
2024-09-25 01:04:10 +0700

    modified:   commit.bat 	modified:   huy.bat 	modified:   tu.bat

--- commit.bat
+++ commit.bat
@@ -1,3 +1,3 @@
 @echo on
 :: Pull the latest changes from the main branch (Keo nhung thay doi moi nhat tu nhanh main)
-git pull origin main && echo -----> Enter your commit message (Nhap tin nhan commit): && set /p commitMessage="Enter your commit message: (Nhap tin nhan commit): " && echo -----> Staging all changes (Them tat ca cac thay doi) && git add . && echo -----> Committing changes (Commit thay doi) && git commit -m "%commitMessage%" && echo -----> Pushing changes to the current branch (Day cac thay doi len nhanh hien tai) && git push
+git pull origin main && echo  Enter your commit message (Nhap tin nhan commit): && set /p commitMessage="Enter your commit message: (Nhap tin nhan commit): " && echo  Staging all changes (Them tat ca cac thay doi) && git add . && echo  Committing changes (Commit thay doi) && git commit -m "%commitMessage%" && echo  Pushing changes to the current branch (Day cac thay doi len nhanh hien tai) && git push
